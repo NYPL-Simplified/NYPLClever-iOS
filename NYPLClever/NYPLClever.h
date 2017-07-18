5767082f13a500f608890e4582abe29b0fbc80b4
Winnie Quinn <me@winniequinn.com>
2017-07-18 17:06:24 -0400

    Format NYPLClever.h

--- NYPLClever/NYPLClever.h
+++ NYPLClever/NYPLClever.h
@@ -6,6 +6,5 @@ FOUNDATION_EXPORT double NYPLCleverVersionNumber;
 //! Project version string for NYPLClever.
 FOUNDATION_EXPORT const unsigned char NYPLCleverVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <NYPLClever/PublicHeader.h>
-
-
+// In this header, you should import all the public headers of your framework using statements like
+// #import <NYPLClever/PublicHeader.h>
