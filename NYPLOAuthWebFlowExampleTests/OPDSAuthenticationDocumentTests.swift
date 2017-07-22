2470e797769e8387d70b29711ca6f4d447e225a9
Winnie Quinn <me@winniequinn.com>
2017-07-21 20:42:53 -0400

    Improve coverage for OPDSAuthenticationDocument

--- NYPLOAuthWebFlowExampleTests/OPDSAuthenticationDocumentTests.swift
+++ NYPLOAuthWebFlowExampleTests/OPDSAuthenticationDocumentTests.swift
@@ -16,7 +16,27 @@ class OPDSAuthenticationDocumentTests: XCTestCase {
     XCTAssertNotNil(OPDSAuthenticationDocument(jsonObject: object("OPDSAuthenticationDocumentGood")))
   }
 
-  func testBad() {
-    XCTAssertNil(OPDSAuthenticationDocument(jsonObject: object("OPDSAuthenticationDocumentBad")))
+  func testNotAnObject() {
+    XCTAssertNil(OPDSAuthenticationDocument(jsonObject: "foo"))
+  }
+
+  func testNoID() {
+    XCTAssertNil(OPDSAuthenticationDocument(jsonObject: object("OPDSAuthenticationDocumentNoID")))
+  }
+
+  func testNoLinks() {
+    XCTAssertNotNil(OPDSAuthenticationDocument(jsonObject: object("OPDSAuthenticationDocumentNoLinks")))
+  }
+
+  func testBadMethods() {
+    XCTAssertNil(OPDSAuthenticationDocument(jsonObject: object("OPDSAuthenticationDocumentBadMethods")))
+  }
+
+  func testBadProvider() {
+    XCTAssertNil(OPDSAuthenticationDocument(jsonObject: object("OPDSAuthenticationDocumentBadProvider")))
+  }
+
+  func testBadProviders() {
+    XCTAssertNil(OPDSAuthenticationDocument(jsonObject: object("OPDSAuthenticationDocumentBadProviders")))
   }
 }
